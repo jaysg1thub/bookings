5161816f5243c55bc726fbfd3a852d6b1016a61b
jaysg1thub <jay@Jays-MacBook-Pro.local>
2025-06-10 23:22:13 -0700

    added .DS_store to .gitignore

---
+++ cmd/web/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/jaysg1thub/bookings/pkg/config"
+	"github.com/jaysg1thub/bookings/pkg/handlers"
+	"github.com/jaysg1thub/bookings/pkg/render"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main application function:
+func main() {
+
+	// chanbge this to true when in Production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour // sessions last for 24 hours
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction // in Prod this will be "true"
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	// creates repository variable
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	// gives render pkg access to the "config.AppConfig" info
+	render.NewTemplates(&app)
+
+	//fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	fmt.Printf("Starting application on port %s", portNumber)
+
+	// _ = http.ListenAndServe(portNumber, nil)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+
+}
---
+++ cmd/web/routes.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/jaysg1thub/bookings/pkg/config"
+	"github.com/jaysg1thub/bookings/pkg/handlers"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	// mux := pat.New()
+
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(SessionLoad)
+
+	// no longer using:
+	//mux.Use(WriteToConsole)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
+
+	return mux
+
+}
